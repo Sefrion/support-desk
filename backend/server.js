40882046bf46befe358d5a49bf2410506737d154
Sefrion <ilyenkov96@gmail.com>
2024-01-31 11:17:03 +0200

    Express server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log('Back-end');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.status(200).json({ message: 'Welcome to Support Desk API' });
+});
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
