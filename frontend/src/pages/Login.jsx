230b03d9155c4773de5a71d929f0e0f463893f46
Sefrion <ilyenkov96@gmail.com>
2024-02-08 10:21:11 +0200

    Hooking Up To Redux

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { FaSignInAlt } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { login } from '../features/auth/authSlice';
 
 function Login() {
 	const [formData, setFormData] = useState({
@@ -9,8 +11,21 @@ function Login() {
 
 	const { email, password } = formData;
 
+	const dispatch = useDispatch();
+
+	const { user, isLoading, isSuccess, message } = useSelector(
+		(state) => state.auth
+	);
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
+
+		const userData = {
+			email,
+			password
+		};
+
+		dispatch(login(userData));
 	};
 
 	const handleChange = (e) => {
